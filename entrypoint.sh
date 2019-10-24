484007fc21eb13b44d66e99d2f99d24fb84e730d
pghildiy <pghildiyal82@gmail.com>
2019-10-24 12:04:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ done
 if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
   echo "Database $PGDATABASE does not exist. Creating..."
   createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
-  mix ecto.migrate
+  mix ecto.setup
   mix run priv/repo/seeds.exs
   echo "Database $PGDATABASE created."
 fi
