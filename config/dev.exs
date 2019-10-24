4ba8e75e2a88a9c7f5756bc579e6763423bfaa42
pghildiy <pghildiyal82@gmail.com>
2019-10-24 14:54:54 +0530

    picking db config from env config

--- config/dev.exs
+++ config/dev.exs
@@ -49,8 +49,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :todo_app, TodoApp.Repo,
-  username: "postgres",
-  password: "p0stgr3s",
-  database: "postgres",
-  hostname: "demo-postgres-demo-postgresql.demo",
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  database: System.get_env("PGDATABASE"),
+  hostname: System.get_env("PGHOST"),
   pool_size: 10
